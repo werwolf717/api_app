3da6808412b7362e60e4e65ccfc20478f15919ff
werwolf <sergei.werwolf@yandex.ru>
2020-06-28 02:25:19 +0300

    Research views/controllers

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+
+@section('title-block')
+        Home
+@endsection
+@section('content')
+    <h1>Main page</h1>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title-block')</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
